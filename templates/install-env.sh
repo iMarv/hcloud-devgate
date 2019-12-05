3250f77a4e6e281183f2d10e2923b87c0bcd5298
Marvin Altemeier <marvin.altemeier@hetzner-cloud.de>
2019-12-05 13:20:49 +0100

    Move installing packets into install-env

--- templates/install-env.sh
+++ templates/install-env.sh
@@ -6,12 +6,15 @@ current_env="${devenv}"
 # Node
 if [ "$current_env" == "node" ]
 then
+    sudo apt-get install yarn -y
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+    nvm install --lts
 fi
 
 # Rust
 if [ "$current_env" == "rust" ]
 then
+    sudo apt-get install libsqlite3-dev -y
     curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly --profile=default --component rls
     source $HOME/.cargo/env && cargo install diesel_cli --no-default-features --features "sqlite"
 fi
@@ -19,5 +22,6 @@ fi
 # Clojure
 if [ "$current_env" == "clojure" ]
 then
+    sudo apt-get install openjdk-8-jdk-headless leiningen -y
     curl -o- https://download.clojure.org/install/linux-install-1.10.1.492.sh | sudo bash
 fi
